c865164d1dee83bbcd28d94a60cf21465d2f68a8
Dagdon <dagdonjephthah@gmail.com>
2024-05-02 08:29:36 -0700

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, div, mul
+    from calculator_1 import add, sub, mul, div
     from sys import argv
     num_args = len(argv)
     if num_args != 4:
