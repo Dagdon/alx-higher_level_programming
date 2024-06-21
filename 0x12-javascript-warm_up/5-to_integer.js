6d411e4398c2d36c6b84b39190e0ccce47a59945
Dagdon <dagdonjephthah.com>
2024-06-21 10:07:59 -0700

    convert to int or print NaN

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,9 @@
 /* converts string to an integer or prints NaN */
 
 const args = process.argv[2];
+i
 if (args === undefined || isNaN(parseInt(args))) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(args));
 }
