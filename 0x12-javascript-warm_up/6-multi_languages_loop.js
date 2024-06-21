5cd59900c84b11a4d8743084a4a9b7fd7c333e99
Dagdon <dagdonjephthah.com>
2024-06-21 10:25:40 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* print a three line message using an array of loop */
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
