0fd51530cc5e867da745381f5bd3868f134f6ea1
Dagdon <dagdonjephthah.com>
2024-07-06 13:00:23 -0700

    a class inheriting from one and printing from another

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* create a class square that extends the rectangle class */
+
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
