4f5342237a3d463cf1f4f7562cc9ac7b5b6d789c
Dagdon <dagdonjephthah.com>
2024-07-08 06:27:51 -0700

    import and compute an array

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* imputes an array and computes a new array */
+
+const list = require('./100-data.js').list;
+
+const newList = list.map((value, index) => value * index);
+
+console.log(list);
+console.log(newList);
