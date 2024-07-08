62b2c28ea3200f8eb982d6cc9afe186d9b1d6ec7
Dagdon <dagdonjephthah.com>
2024-07-08 08:46:13 -0700

    concat two files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -17,14 +17,12 @@ fs.readFile(file1Path, 'utf8', (err1, data1) => {
     console.error(`Error reading ${file1Path}: ${err1}`);
     return;
   }
-
   fs.readFile(file2Path, 'utf8', (err2, data2) => {
     if (err2) {
       console.error(`Error reading ${file2Path}: ${err2}`);
       return;
     }
-    const concatenatedContent = data1 + '\n' + data2;
-
+    const concatenatedContent = (data1.trim() + data2.trim()).trim();
     fs.writeFile(destinationPath, concatenatedContent, (err3) => {
       if (err3) {
         console.error(`Error writing to ${destinationPath}: ${err3}`);
