5ae22de604d29a417ff2c5ebfc997ab4389c4165
Dagdon <dagdonjephthah.com>
2024-07-08 01:58:51 -0700

    a function that returns the reversed version of a list:

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* a function that returns the reversed version of a list */
+
+exports.esrever = function (list) {
+	let new_list = [];
+	for (let i = list.length - 1; i >= 0; i--) {
+		new_list.push(list[i]);
+	}
+	return new_list;
+};
