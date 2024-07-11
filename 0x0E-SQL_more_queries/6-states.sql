623dc052bdb3bb5afa20a37c9f452a0b646ad6e7
Dagdon <dagdonjephthah@gmail.com>
2024-07-11 09:22:22 -0700

    create databse and a table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,11 +1,11 @@
 -- create a database
 -- create a table in the database
 
-CREATE DATABASE IF NOT EXITS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	VARCHAR(256) can’t be null
-)
+	VARCHAR(256) NOT NULL
+);
