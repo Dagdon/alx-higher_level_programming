2e4a5d3b2518936976c2be21518f7316af4090c6
Dagdon <dagdonjephthah@gmail.com>
2024-07-11 10:08:11 -0700

    list cities of california in the database

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,6 +3,6 @@
 
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.states_id = states.id
-AND staes.name = 'California'
+WHERE cities.state_id = states.id
+AND states.name = 'California'
 ORDER BY cities.id ASC;
