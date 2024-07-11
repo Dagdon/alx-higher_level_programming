90a04c8985e9f103eda10faf087a3d5452e0c87b
Dagdon <dagdonjephthah@gmail.com>
2024-07-11 08:48:04 -0700

    show all user privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,9 @@
 -- list all privileges of a user
 -- in an SQL server
 
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
+
 SELECT user, host, privilege_type
 FROM information_schema.user_privileges
 WHERE user IN ('user_0d_1', 'user_0d_2')
