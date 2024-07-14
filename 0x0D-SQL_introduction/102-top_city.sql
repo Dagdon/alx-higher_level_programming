4bd69721407c73a67382354ae130ab05ccbfb649
Dagdon <dagdonjephthah@gmail.com>
2024-07-14 05:01:08 -0700

    dispaly top three cities with temp in july

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,10 @@
+-- displays the top three cities temprature in july
+
+USE hbtn_0c_0;
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+HERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC;
+LIMIT 3;
