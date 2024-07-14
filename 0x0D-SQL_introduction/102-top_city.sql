7367ae6f0cd69add12235b528e3b5528ad9c938f
Dagdon <dagdonjephthah@gmail.com>
2024-07-14 05:04:23 -0700

    dispaly top three cities with temp in july

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -4,7 +4,7 @@ USE hbtn_0c_0;
 
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-HERE month = 7 OR month = 8
+WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC
 LIMIT 3;
