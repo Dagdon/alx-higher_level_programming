7623b256ba3f547303adf75f3fe1a92ffe76b8a5
Dagdon <dagdonjephthah@gmail.com>
2024-07-23 06:03:45 -0700

    create a class and initialize

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""Module that defines a Square class."""
+
 
 class Square:
     """Defines a square by its size."""
 
 
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the new square.
+        """
         self.__size = size
